8252908f57012806c8f139a959d840fa7ec52f59
mohammadali95 <35858726+mohammadali95@users.noreply.github.com>
2018-04-20 10:14:59 -0500

    Data structure for Password label

---
+++ src/Unsorted/PasswordLabel.java
@@ -0,0 +1,20 @@
+package Unsorted;
+
+public class PasswordLabel {
+	private String label;
+	private String password;
+	 
+	  public PasswordLabel(String label, String password) {
+	    this.label = label;
+	    this.password = password;
+	  }
+	  
+	  public String getLabel() {
+		  return label;
+	  }
+	  
+	  public String getPassword() {
+		  return password;
+	  }
+
+}
