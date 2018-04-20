adae72238d64d7f5a949001547785c65b98dad3d
mohammadali95 <35858726+mohammadali95@users.noreply.github.com>
2018-04-20 10:14:25 -0500

    Updated Controller

--- src/Unsorted/Controller.java
+++ src/Unsorted/Controller.java
@@ -1,109 +1,184 @@
 package Unsorted;
 import OtherStuff.PasswordGenerator;
+import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.util.Callback;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Controller {
 
-    @FXML
-    Button popup;
-
-    @FXML
-    TableView table;
-
-    @FXML
-    TableColumn label;
-
-    @FXML
-    TableColumn password;
-
-    @FXML
-    TableColumn editButton;
-
-    @FXML
-    TableColumn deleteButton;
-
-    @FXML
-    TextField labelField;
-
-    @FXML
-    TextField passwordField;
-
-  Database database;
-
-    PasswordGenerator generator;
-
-    ArrayList<Tuple<String>> data;
 
 
+	@FXML
+	Button popup;
+	
+	@FXML
+	Button addPasswordButton;
 
-    @FXML
-    private void initialize() {
-        generator = new PasswordGenerator();
+	@FXML
+	TableView<PasswordLabel> table;
 
-//        popup.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                final Stage dialog = new Stage();
-//                dialog.initModality(Modality.APPLICATION_MODAL);
-//                dialog.initOwner(Main.getStage());
-//                VBox dialogVbox = new VBox(20);
-//
-//
-//                Scene dialogScene = new Scene(dialogVbox, 500, 300);
-//                dialog.setScene(dialogScene);
-//                dialog.show();
-//            }
-//        });
-    }
+	@FXML
+	TableColumn<PasswordLabel, String> label;
 
-    @FXML
-    private void generate() {
-        // TODO:
-        // pull up popup with checkboxes for generation parameters
-        // change parameters in this.generator after user clicks 'save'
-        // get new password with generator.generate()
-        // store password in passwordField
-    }
+	@FXML
+	TableColumn <PasswordLabel, String> password;
 
-    @FXML
-    private void addPassword() {
-        // TODO  make sure there is something in labelField & passwordField & that labelField is unique in this.data
-//        database.addPassword(new Tuple<String>());
-    }
+	@FXML
+	TableColumn editButton;
 
-    @FXML
-    private void editLabel() {
-        // TODO turn label in row into an editable textfield
-        // change data in database
-        // update table
-    }
+	@FXML
+	TableColumn deleteButton;
+	
+	@FXML
+	Button generateButton;
 
-    @FXML
-    private void editPassword() {
-        // TODO  turn password label in row into editable textfield
-        // change data in database
-        // update table
-    }
+	@FXML
+	TextField labelField;
 
-    @FXML
-    private void delete() {
-        // TODO delete password from database using database.delete(oldlabel/password, newlabel/password)
-    }
+	@FXML
+	TextField passwordField;
 
-    @FXML
-    private void show() {
-        // TODO password from clicked-on row
-    }
+	Database database;
 
-    private void populateTable() {
-        // TODO clear table & repopulate with database info
-        // *** out passwords
+	PasswordGenerator generator;
 
-    }
+	ArrayList<Tuple<String>> data;
+
+
+
+	@FXML
+	private void initialize() throws SQLException, Exception {
+		System.out.println("hi");
+		label.setCellValueFactory(cdf -> new SimpleStringProperty(cdf.getValue().getLabel()));
+		password.setCellValueFactory(cdf -> new SimpleStringProperty(cdf.getValue().getPassword()));
+				
+		generator = new PasswordGenerator();
+		database = new Database();
+		database.createDatabase();
+		data = database.getAllPasswords();
+		for (Tuple t: data) {
+			table.getItems().add(new PasswordLabel ((String)t.getLabel(), (String)t.getPassword()));
+		}
+		System.out.println(data.toString());
+		System.out.println(generateButton == null);
+		
+		Platform.runLater(() -> {generateButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// TODO Auto-generated method stub
+				String genPassword = generator.generate();
+				passwordField.setText(genPassword);
+				
+			}
+			
+			
+		});});
+		
+
+		/*
+        table.getItems().add(new PasswordLabel(label.getText(), 
+                password.getText()));
+        label.setText("asds");
+        password.setText("asdsa");
+		 */
+
+
+		//        popup.setOnAction(new EventHandler<ActionEvent>() {
+		//            @Override
+		//            public void handle(ActionEvent event) {
+		//                final Stage dialog = new Stage();
+		//                dialog.initModality(Modality.APPLICATION_MODAL);
+		//                dialog.initOwner(Main.getStage());
+		//                VBox dialogVbox = new VBox(20);
+		//
+		//
+		//                Scene dialogScene = new Scene(dialogVbox, 500, 300);
+		//                dialog.setScene(dialogScene);
+		//                dialog.show();
+		//            }
+		//        });
+	}
+	
+	private void update() throws SQLException, Exception {
+		table.getItems().clear();
+		data = database.getAllPasswords();
+		for (Tuple t: data) {
+			table.getItems().add(new PasswordLabel ((String)t.getLabel(), (String)t.getPassword()));
+		}
+		System.out.println(data.toString());
+		
+	}
+
+	@FXML
+	private void generate() {
+		// TODO:
+		// pull up popup with checkboxes for generation parameters
+		// change parameters in this.generator after user clicks 'save'
+		// get new password with generator.generate()
+		// store password in passwordField
+	}
+
+	@FXML
+	private void addPassword() throws SQLException, Exception {
+		// TODO  make sure there is something in labelField & passwordField & that labelField is unique in this.data
+		//        database.addPassword(new Tuple<String>());
+		try {
+			database.addPassword(new Tuple(labelField.getText(), passwordField.getText()));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		labelField.clear();
+		passwordField.clear();
+		update();
+	}
+
+	@FXML
+	private void editLabel() {
+		// TODO turn label in row into an editable textfield
+		// change data in database
+		// update table
+	}
+
+	@FXML
+	private void editPassword() {
+		// TODO  turn password label in row into editable textfield
+		// change data in database
+		// update table
+	}
+
+	@FXML
+	private void delete() {
+		// TODO delete password from database using database.delete(oldlabel/password, newlabel/password)
+		try {
+			database.deletePassword(new Tuple(labelField.getText(), passwordField.getText()));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@FXML
+	private void show() {
+		// TODO password from clicked-on row
+	}
+
+	private void populateTable() {
+		// TODO clear table & repopulate with database info
+		// *** out passwords
+
+	}
 }
