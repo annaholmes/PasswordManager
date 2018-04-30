c7e7c24f25d40101a5aa13324c2c98a7eb1a5c21
Unknown <samantha.wycoff1@yahoo.com>
2018-04-29 23:07:44 -0500

    More Database Methods

--- src/Unsorted/Database.java
+++ src/Unsorted/Database.java
@@ -1,26 +1,62 @@
 package Unsorted;
 import Encryption.Encryption;
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Database {
-	private Connection con;
-	private Statement stat;
-	private String masterPassword;
+	private static Connection con;
+	private static Statement stat;
+	private static String masterPassword;
 	
-	public Database() {
-		this.masterPassword = "abc";
-	}
-	
-    public void createDatabase() throws SQLException, ClassNotFoundException {
+    public static void createDatabase() throws SQLException, ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
         con = DriverManager.getConnection("jdbc:sqlite:Passwords");
         stat = con.createStatement();
         stat.execute("CREATE TABLE IF NOT EXISTS Passwords (Label TEXT, Password TEXT)");
-        stat.execute("CREATE TABLE IF NOT EXISTS Master (Password TEXT)");
+        stat.execute("CREATE TABLE IF NOT EXISTS Master (Password TEXT, Tries INTEGER, Next Attempt TIMESTAMP)");
+    }
+    
+    public static void setMasterPassword(String password) {
+    	masterPassword = password;
+    }
+    
+    public static void setMasterTable(int tries) throws SQLException {
+    	PreparedStatement add = con.prepareStatement("INSERT INTO Master VALUES (?, ?, ?)");
+    	add.setString(1, masterPassword);
+    	add.setInt(2, tries);
+    	Calendar calendar = Calendar.getInstance();
+    	add.setTimestamp(3, new java.sql.Timestamp(calendar.getTimeInMillis()));
+    	add.execute();
+    }
+    
+    public static int attemptedTries() throws SQLException {
+    	stat.execute("SELECT Tries FROM Master");
+    	ResultSet results = stat.getResultSet();
+    	int tries = results.getInt(1);
+    	results.close();
+    	return tries;
+    }
+    
+    public static void triesMaxed() throws SQLException {
+    	PreparedStatement update = con.prepareStatement("UPDATE Master SET Tries = ?, Next Attempt = ?");
+    	update.setInt(1, 0);
+    	Calendar calendar = Calendar.getInstance();
+    	calendar.add(30, Calendar.MINUTE);
+    	update.setTimestamp(2, new java.sql.Timestamp(calendar.getTimeInMillis()));
+    	update.execute();
+    }
+    
+    public static LocalDateTime nextLoginTime() throws SQLException {
+    	stat.execute("SELECT Next Attempt FROM Master");
+    	ResultSet results = stat.getResultSet();
+    	Timestamp date = results.getTimestamp(1);
+    	results.close();
+    	return date.toLocalDateTime();
     }
 
-    public void addPassword(Tuple<String> labelAndPassword) throws Exception {
+    public static void addPassword(Tuple<String> labelAndPassword) throws Exception {
         Tuple<String> encrypted = Encryption.encryptTuple(labelAndPassword, masterPassword);
         PreparedStatement search = con.prepareStatement("SELECT * FROM Passwords WHERE Label = ?");
         search.setString(1, encrypted.getLabel());
@@ -34,7 +70,7 @@ public class Database {
         results.close();
     }
 
-    public ArrayList<Tuple<String>> getAllPasswords() throws SQLException, Exception {
+    public static ArrayList<Tuple<String>> getAllPasswords() throws SQLException, Exception {
         ArrayList<Tuple<String>> passwords = new ArrayList<Tuple<String>>();
         stat.execute("SELECT * FROM Passwords");
         ResultSet results = stat.getResultSet();
@@ -45,7 +81,7 @@ public class Database {
         return passwords;
     }
     
-    public ArrayList<Tuple<String>> searchPasswords(String label) throws Exception {
+    public static ArrayList<Tuple<String>> searchPasswords(String label) throws Exception {
     	ArrayList<Tuple<String>> passwords = new ArrayList<Tuple<String>>();
     	PreparedStatement search = con.prepareStatement("SELECT * FROM Passwords WHERE Label = ?");
     	search.setString(1, label);
@@ -57,7 +93,7 @@ public class Database {
         return passwords;
     }
 
-    public void editData(Tuple<String> oldLabelAndPassword, Tuple<String> newLabelAndPassword) throws Exception {
+    public static void editData(Tuple<String> oldLabelAndPassword, Tuple<String> newLabelAndPassword) throws Exception {
         oldLabelAndPassword = Encryption.encryptTuple(oldLabelAndPassword, masterPassword);
         newLabelAndPassword = Encryption.encryptTuple(newLabelAndPassword, masterPassword);
         PreparedStatement edit = con.prepareStatement("SELECT * FROM Passwords WHERE Label = ?");
@@ -72,7 +108,7 @@ public class Database {
         }
     }
 
-    public void deletePassword(Tuple<String> labelAndPassword) throws Exception {
+    public static void deletePassword(Tuple<String> labelAndPassword) throws Exception {
         labelAndPassword = Encryption.encryptTuple(labelAndPassword, masterPassword);
         PreparedStatement search = con.prepareStatement("SELECT * FROM Passwords WHERE Label = ?");
         search.setString(1, labelAndPassword.getLabel());
@@ -83,5 +119,11 @@ public class Database {
             delete.execute();
         }
         results.close();
+    }
+    
+    public static void dropDatabase() throws SQLException {
+    	stat.execute("DROP DATABASE Passwords");
+    	stat.close();
+    	con.close();
     }
 }
