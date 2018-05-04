afd94c9185597ace0470c5d12da8fd55b4856d65
Unknown <samantha.wycoff1@yahoo.com>
2018-05-04 08:25:24 -0500

    Works on My Computer!!

--- src/OtherStuff/PasswordGenerator.java
+++ src/OtherStuff/PasswordGenerator.java
@@ -1,4 +1,4 @@
-package Passwords;
+package OtherStuff;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.stream.IntStream;
--- src/OtherStuff/RainbowTables.java
+++ src/OtherStuff/RainbowTables.java
@@ -1,4 +1,4 @@
-package Passwords;
+package OtherStuff;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
