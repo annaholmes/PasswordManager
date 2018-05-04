33c6a26bddeef7ea932110bbd49d8ec1722b8673
Unknown <samantha.wycoff1@yahoo.com>
2018-05-04 05:04:06 -0500

    PasswordLabel Test

--- src/Data/PasswordLabel.java
+++ src/Data/PasswordLabel.java
@@ -3,54 +3,37 @@ package Data;
 public class PasswordLabel {
 	private String label;
 	private String password;
+	private boolean isHidden;
+
+	public PasswordLabel(String label, String password, boolean isHidden) {
+		this.label = label;
+		this.password = password;
+		this.isHidden = isHidden;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public String getPassword() {
+		if (isHidden) {
+			return "********";
+		} else {
+			return password;
+		}
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+		isHidden = false;
+
+	}
 
 	public boolean isHidden() {
 		return isHidden;
 	}
 
-	//private Button edit;
-	private boolean isHidden;
-	//private Button delete;
-	
-	 
-	 
-	public PasswordLabel(String label, String password, boolean isHidden) {
-	    this.label = label;
-	    this.password = password;
-	    this.isHidden = isHidden;
-	    //this.edit = new Button("edit");
-	    //this.edit = new Button("delete");
-	    
+	public void toggleHidden(){
+		isHidden = !isHidden;
 	}
-	  
-	  public String getLabel() {
-		  return label;
-	  }
-	  
-	  public String getPassword() {
-		  if (isHidden) {
-			  return "********";
-		  } else {
-			  return password;
-		  }
-	  }
-	  
-	  public void setPassword(String password) {
-		  this.password = password;
-		  isHidden = false;
-
-	  }
-	  
-	  public void toggleHidden(){
-		 isHidden = !isHidden;
-	  }
-	  
-	  public String toString() {
-	  	if (isHidden) {
-	  		return "********";
-		} else {
-	  		return password;
-		}
-	  }
-
 }
--- src/Data/PasswordLabelTest.java
+++ src/Data/PasswordLabelTest.java
@@ -5,10 +5,22 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PasswordLabelTest {
-
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetters() {
+		PasswordLabel testPassword = new PasswordLabel("Facebook", "password", false);
+		assertTrue(testPassword.getLabel().equals("Facebook"));
+		assertTrue(testPassword.getPassword().equals("password"));
+		assertTrue(testPassword.isHidden() == false);
+		testPassword.toggleHidden();
+		assertTrue(testPassword.getPassword().equals("********"));
+		assertTrue(testPassword.isHidden() == true);
 	}
-
+	
+	@Test
+	public void testSetter() {
+		PasswordLabel testPassword = new PasswordLabel("Facebook", "password", true);
+		testPassword.setPassword("newPassword");
+		assertTrue(testPassword.getPassword().equals("newPassword"));
+		assertTrue(testPassword.isHidden() == false);
+	}
 }
